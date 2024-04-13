003f03cf8a89c183319d84beb341e136ab5f2946
Gautam Kumar <146861047+gautamdesigns@users.noreply.github.com>
2024-04-13 20:05:41 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-window.onload = function () {
-	document.getElementById("preloader").style.display = "none";
-	document.getElementById("container").style.display = "block";
+let searchBox = document.querySelector(".search-box");
+document.querySelector("#search-btn").onclick = () => {
+	searchBox.classList.toggle("active");
+};
+let shoppingCart = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-cart-btn").onclick = () => {
+	shoppingCart.classList.toggle("active");
 };
