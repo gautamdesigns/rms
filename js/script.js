8a4945974eff5f065c5f8f4814e43eea7f92666a
Gautam Kumar <146861047+gautamdesigns@users.noreply.github.com>
2024-04-16 20:36:35 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,39 @@
 let searchBox = document.querySelector(".search-box");
 document.querySelector("#search-btn").onclick = () => {
 	searchBox.classList.toggle("active");
+	shoppingCart.classList.remove("active");
+	signIn.classList.remove("active");
+	menu.classList.remove("active");
 };
 let shoppingCart = document.querySelector(".shopping-cart");
 document.querySelector("#shopping-cart-btn").onclick = () => {
 	shoppingCart.classList.toggle("active");
+	searchBox.classList.remove("active");
+	signIn.classList.remove("active");
+	menu.classList.remove("active");
 };
+let signIn = document.querySelector(".sign-in");
+document.querySelector("#profile-btn").onclick = () => {
+	signIn.classList.toggle("active");
+	searchBox.classList.remove("active");
+	shoppingCart.classList.remove("active");
+	menu.classList.remove("active");
+};
+let menu = document.querySelector(".navbar");
+document.querySelector("#menu-btn").onclick = () => {
+	menu.classList.toggle("active");
+	searchBox.classList.remove("active");
+	shoppingCart.classList.remove("active");
+	signIn.classList.remove("active");
+};
+window.onscroll = () => {
+	searchBox.classList.remove("active");
+	shoppingCart.classList.remove("active");
+	signIn.classList.remove("active");
+	menu.classList.remove("active");
+};
+
+
+
+
+
